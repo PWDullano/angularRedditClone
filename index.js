2b341215b7402d3214e8e2d5e858ec6c29ea76c3
Patrick Dullano <PWDullano@gmail.com>
2016-02-18 19:19:07 -0700

    post fields blank after first submission

--- index.js
+++ index.js
@@ -11,5 +11,9 @@ app.controller("MyController", function($scope){
    post.image = $scope.image;
    post.description = $scope.description;
    $scope.posts.push(post);
+   $scope.title = null;
+   $scope.author = null;
+   $scope.image = null;
+   $scope.description = null;
  }
 });
